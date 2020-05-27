b21c5bd1a180b7590e36f0ef05d18e6de65f52d2
Austin T <austin.taranto@gmail.com>
2020-05-27 16:16:06 -0400

    local storage function

--- app.js
+++ app.js
@@ -22,6 +22,36 @@ function loadEventListeners() {
     filter.addEventListener("keyup", filterTasks);
 }
 
+// get tasks from local storage
+function getTasks() {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function(task) {
+        // create li element
+    const li = document.createElement("li");
+    // add class
+    li.className = "collection-item";
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create new link element 
+    const link = document.createTextNode("a");
+    // add class
+    link.className = "delete-item secondary-content";
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // append the link to li
+    li.appendChild(link);
+
+    // append li to ul
+    taskList.appendChild(li);
+    });
+}
+
 // add task
 function addTask(e) {
     if(taskInput.value === "") {
